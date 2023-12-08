cf4e41394496c0c3f6c84c7772f54d1a46dba8ac
Kaymone1 <kennya.benoit15@gmail.com>
2023-12-08 16:10:07 -0500

    changed model

--- nicuinventory/models.py
+++ nicuinventory/models.py
@@ -13,33 +13,13 @@ class InventoryItem(models.Model):
     reorder_point = models.PositiveIntegerField()
     expiration_date = models.DateField()
 
-    # Location split into 8 sections
-    LOCATION_CHOICES = [
-        ('Section1', 'Section 1'),
-        ('Section2', 'Section 2'),
-        ('Section3', 'Section 3'),
-        ('Section4', 'Section 4'),
-        ('Section5', 'Section 5'),
-        ('Section6', 'Section 6'),
-        ('Section7', 'Section 7'),
-        ('Section8', 'Section 8'),
-    ]
-    location = models.CharField(max_length=10, choices=LOCATION_CHOICES)
+    # Location
+    location = models.CharField(max_length=10)
+    # time stamp
     last_updated = models.DateTimeField(auto_now=True)
-
-    CRITICALITY_CHOICES = [
-        ('High', 'High'),
-        ('Medium', 'Medium'),
-        ('Low', 'Low'),
-    ]
-    criticality_level = models.CharField(max_length=20, choices=CRITICALITY_CHOICES, blank=True, null=True)
+    # how important they are
+    criticality_level = models.CharField(max_length=20, blank=True, null=True)
 # see whats going on with items
-    STATUS_CHOICES = [
-        ('Active', 'Active'),
-        ('Inactive', 'Inactive'),
-        ('Out of Stock', 'Out of Stock'),
-        ('Reorder', 'Reorder'),
-    ]
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=20)
 
 
